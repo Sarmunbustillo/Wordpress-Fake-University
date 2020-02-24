84eea737624378c601846bed6ef4a9514f9cbf91
Sarmunbustillo <sarchamo26@gmail.com>
2020-02-24 10:46:35 +0100

    created new posts

--- Fictional-University-theme/functions.php
+++ Fictional-University-theme/functions.php
@@ -14,11 +14,16 @@
 
     add_action('after_setup_theme', 'university_features');
 
-
+    //manipulate the query
     function university_adjust_queries($query) {
+        if(!is_admin() AND is_post_type_archive('program') AND $query->is_main_query()) {
+            $query->set('orderby','title');
+            $query->set('order', 'ASC');
+            $query->set('posts_per_page', -1);
+        };
+
         if (!is_admin() AND is_post_type_archive('event') AND $query->is_main_query())  {
-            $today = date('Ymd');
-            
+            $today = date('Ymd');            
             $query->set('meta_key', 'event_date');
             $query->set('orderby', 'meta_value_num');
             $query->set('order', 'ASC');
