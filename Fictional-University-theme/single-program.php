475797f1cd7c0939566ef265ce5bfa1ee4e99c0c
Sarmunbustillo <sarchamo26@gmail.com>
2020-02-26 12:24:33 +0100

    added pictures with custom fields and custom sizes

--- Fictional-University-theme/single-program.php
+++ Fictional-University-theme/single-program.php
@@ -23,6 +23,42 @@
         </div>
 
         <?php 
+           $relatedProfessors = new WP_Query(array(
+            'posts_per_page' => -1,
+            'post_type' => 'professor',
+            'orderby' => 'title',
+            'order' => 'ASC',
+            'meta_query' => array(              
+              array(
+                'key' => 'related_programs',
+                'compare' => 'LIKE',
+                'value' => '"' . get_the_ID() . '"'
+              )
+            )
+          ));
+
+          if($relatedProfessors->have_posts()) {
+            
+            echo '<hr class="section-break">';
+            echo '<h2 class="headline headline--medium">' . get_the_title() . ' Professors</h2>';
+            
+            echo '<ul class="professor-cards">';
+            while($relatedProfessors->have_posts()) {
+              $relatedProfessors->the_post(); ?>
+              <li class="professor-card__item">
+                <a class="professor-card" href="<?php the_permalink(); ?>">
+                  <img class="professor-card__image" src="<?php the_post_thumbnail_url('professor-landscape'); ?>">
+                  <span class="professor-card__name"><?php the_title(); ?></span>
+                </a>
+              </li>
+            <?php }  
+            echo '</ul>';
+          }
+
+          wp_reset_postdata();
+
+
+
           $today = date('Ymd');
           $homepageEvents = new WP_Query(array(
             'posts_per_page' => 2,
@@ -47,7 +83,7 @@
           ));
 
           if($homepageEvents->have_posts()) {
-            
+
             echo '<hr class="section-break">';
             echo '<h2 class="headline headline--medium">Upcoming ' . get_the_title() . ' Events</h2>';
   
@@ -72,7 +108,6 @@
                 </div>
               </div>
             <?php }  
-
           }
                   
         ?>       
